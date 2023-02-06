13b1d6df9967f6bda771ac01c0ae69238c0def19
Matthew Wiram <matthew.wiram551@gmail.com>
2023-02-06 11:33:56 -0500

    user service and user repository

--- src/main/java/com/javalocity/javalocity/repository/UserRepository.java
+++ src/main/java/com/javalocity/javalocity/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.javalocity.javalocity.repository;
 
-public class UserRepository {
+import com.javalocity.javalocity.bean.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User getById(long id);
+    User findByUsername(String name);
 }
