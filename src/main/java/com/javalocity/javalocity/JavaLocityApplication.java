6d11cdc9e8fefce2de1990dca3ac2b038338fb89
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2023-02-06 08:39:57 -0700

    first commit

---
+++ src/main/java/com/javalocity/javalocity/JavaLocityApplication.java
@@ -0,0 +1,13 @@
+package com.javalocity.javalocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLocityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLocityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javalocity/javalocity/JavaLocityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalocity.javalocity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLocityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
