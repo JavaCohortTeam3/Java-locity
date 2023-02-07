a13454a2f299c945d78944b27619ceeb88ea1271
Matthew Wiram <matthew.wiram551@gmail.com>
2023-02-07 12:47:45 -0500

    got the trip/locations done

--- src/main/java/com/javalocity/javalocity/controller/TripController.java
+++ src/main/java/com/javalocity/javalocity/controller/TripController.java
@@ -1,7 +1,32 @@
 package com.javalocity.javalocity.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TripController {
+
+    @GetMapping("/trip/locations")
+    public String findLocale() {
+        return "/trip-location";
+    }
+    @PostMapping("/trip/locations")
+    public String getLocale(@RequestParam("location") String location, Model model, HttpSession session) {
+        System.out.println(location);
+
+        session.setAttribute("location", location);
+        return "redirect:/trip/details";
+    }
+    @GetMapping("/trip/details")
+    public String getDetails(Model model, HttpSession session) {
+
+        model.addAttribute("location", session.getAttribute("location"));
+
+
+        return "/trip-details";
+    }
 }
---
+++ src/main/resources/static/JS/locationAPIcall.js
@@ -0,0 +1,66 @@
+"use strict"
+
+let token = "pk.eyJ1IjoibWF0dGhld3dpcmFtIiwiYSI6ImNsOWx2YmJwODFtMnEzdXAyMDFvdHRxcHcifQ.20mYzJo1wfnNRyCTEJMtyw"
+
+
+
+
+function geocode(search, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+            // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            console.log(data.features)
+
+            return data.features;
+        });
+}
+
+mapboxgl.accessToken = token;
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    zoom: 19,
+    center: [-80.175652, 33.018505]
+
+});
+
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+);
+
+
+let input = document.getElementsByClassName("mapboxgl-ctrl-geocoder--input")
+
+
+
+
+    console.log(input[0].value)
+let submit = document.getElementById("submission")
+submit.addEventListener("click", function (e) {
+
+    e.preventDefault()
+    let data = geocode(input[0].value, token);
+    data.then(res => {
+        document.getElementById("location").value = res[0].center[1] + "," +res[0].center[0]
+        console.log(document.getElementById("location").value);
+        document.getElementsByTagName("form")[1].submit()
+    })
+})
+
+
+
+
+
+
+
+
+
+
