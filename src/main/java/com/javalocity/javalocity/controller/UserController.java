afdee6303003dfa8ea72e65e4fea768b25b17f27
Dustin Zook <dustin.w.zook@gmail.com>
2023-02-08 10:16:31 -0600

    finished 500 error page

--- src/main/java/com/javalocity/javalocity/controller/UserController.java
+++ src/main/java/com/javalocity/javalocity/controller/UserController.java
@@ -10,11 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-<<<<<<< HEAD
-
-=======
-import org.springframework.web.bind.annotation.RequestParam;
->>>>>>> fce87cfbc0d2d8f60394c42b7f481fdb95953164
+//<<<<<<< HEAD
+//
+//=======
+//import org.springframework.web.bind.annotation.RequestParam;
+//>>>>>>> fce87cfbc0d2d8f60394c42b7f481fdb95953164
 
 //import org.springframework.security.crypto.password.PasswordEncoder;
 @Controller
